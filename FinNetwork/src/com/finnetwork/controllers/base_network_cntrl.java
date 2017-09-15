c542327f41e07273fad95ba67a170522999190ac
sandunika <sandunikahansinie@gmail.com>
2017-09-15 14:44:33 +0530

    Retrived Nodes and Links for a given year

--- FinNetwork/src/com/finnetwork/controllers/base_network_cntrl.java
+++ FinNetwork/src/com/finnetwork/controllers/base_network_cntrl.java
@@ -6,23 +6,60 @@ import org.hibernate.Session;
 import org.hibernate.query.Query;
 
 import com.finnetwork.models.FeiiiInitData;
+import com.finnetwork.models.Link;
+import com.finnetwork.models.Node;
 import com.finnetwork.persistence.hibernate_util;
 
 public class base_network_cntrl {
 	
-	public List<FeiiiInitData> get_base_network(int year) {
-		System.out.println("Call for basenetwork controller..");
+	public List<Node> get_base_network(int year) {
+		System.out.println("Call for basenetwork controller...");
 	
 		Session session = hibernate_util.getSessionFactory().openSession();
 		
 		session.beginTransaction();		
 		
-		Query query = session.createQuery("from FeiiiInitData where fillingData = :year ");
-		query.setParameter("year", year);
-		List<FeiiiInitData> list = query.list();
-		System.out.println("size of the selected data list : " +list.size());
+		Query queryLink = session.createQuery("FROM Link WHERE fillingDate LIKE :year");
+		queryLink.setParameter("year", "%"+year);		
+		List<Link> links = queryLink.list();
+		
+		System.out.println("link size : " + links.size());
+		System.out.println(links.get(2).getRole());
+	
+		Query querySource = session.createQuery("SELECT DISTINCT source FROM Link WHERE fillingDate LIKE :year");
+		querySource.setParameter("year", "%"+year);
+		List<Integer> sources  = querySource.list();
+		System.out.println("sources size: " + sources.size());
+		for ( Integer source : sources) {
+			System.out.print(source + ", ");
+		}
+		
+		Query queryTarget = session.createQuery("SELECT DISTINCT source FROM Link WHERE fillingDate LIKE :year");
+		queryTarget.setParameter("year", "%"+year);
+		List<Integer> targets = queryTarget.list();
+		System.out.println("\ntarget size: " + targets.size());
+		for (Integer target : targets) {
+			System.out.print(target + ", ");
+		}
+		
+		sources.removeAll(targets);
+		sources.addAll(targets);
+		
+		System.out.println("\n");
+		for (Integer i : sources) {			
+			System.out.print(i + ", ");
+		}
+		
+		Query queryNodes = session.createQuery("FROM Node WHERE id IN (:ids)");
+		queryNodes.setParameter("ids", sources);
+		List<Node> nodes = queryNodes.list();
+		System.out.println("\nFinal size is : " + nodes.size());
+		for (Node node : nodes) {
+			System.out.println(node.getId() + ", " + node.getEquity());
+		}
+		
 		session.getTransaction().commit();
 		session.close();
-		return list;
+		return nodes;
 	}
 }
--- FinNetwork/src/com/finnetwork/views/base_network.java
+++ FinNetwork/src/com/finnetwork/views/base_network.java
@@ -11,7 +11,7 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import com.finnetwork.controllers.base_network_cntrl;
-import com.finnetwork.models.FeiiiInitData;
+import com.finnetwork.models.Node;
 
 @Path("/base_network")
 public class base_network {
@@ -22,8 +22,8 @@ public class base_network {
 	public Response getResponse (@PathParam("param") int req){
 		
 		base_network_cntrl base_net_cntrl = new base_network_cntrl();
-		List<FeiiiInitData> temp_list = base_net_cntrl.get_base_network(req);
-		GenericEntity<List<FeiiiInitData>> entity = new GenericEntity<List<FeiiiInitData>>(temp_list){};
+		List<Node> temp_list = base_net_cntrl.get_base_network(req);
+		GenericEntity<List<Node>> entity = new GenericEntity<List<Node>>(temp_list){};
 		
 		return Response.status(200).entity(entity).build();	
 	}
