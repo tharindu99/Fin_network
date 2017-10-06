1fdc2a39a52ae5c3453ecd4cb42e8af396a572ae
kushankavinda <kaavinda92@gmail.lk>
2017-10-06 11:32:05 +0530

    retrive json to selected conpany (with using rest api)

---
+++ FinNetwork/src/com/finnetwork/controllers/base_network_cntrl_company.java
@@ -0,0 +1,78 @@
+
+package com.finnetwork.controllers;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.query.Query;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.finnetwork.models.Link;
+import com.finnetwork.models.Node;
+import com.finnetwork.persistence.hibernate_util;
+
+
+public class base_network_cntrl_company {
+	//	take the year parameter and return ObjectNode
+	public ObjectNode get_base_network(String company) {
+		System.out.println("Call for basenetwork controller...");
+		
+		//	Creata a session
+		Session session = hibernate_util.getSessionFactory().openSession();
+		//	Begin transaction
+		session.beginTransaction();		
+		
+		//	Retrieve all the links for a given year
+		Query queryLink = session.createQuery("FROM Link WHERE fillingDate LIKE :company");
+		queryLink.setParameter("company", "%"+company);		
+		List<Link> links = queryLink.list();
+		
+		//	Select all the distinct source nodes from given year
+		Query querySource = session.createQuery("SELECT DISTINCT source FROM Link WHERE fillingDate LIKE :company");
+		querySource.setParameter("company", "%"+company);
+		List<Integer> sources  = querySource.list();
+		
+		//	Select all the distinct target nodes from given year
+		Query queryTarget = session.createQuery("SELECT DISTINCT target FROM Link WHERE fillingDate LIKE :company");
+		queryTarget.setParameter("company", "%"+company);
+		List<Integer> targets = queryTarget.list();
+		
+		// 	List of all the nodes for a given year 
+		sources.removeAll(targets);
+		sources.addAll(targets);
+		//	Now all the distinct nodes are stored in sources List
+		
+		//	It is passed to the query to retrieve all the node details
+		Query queryNodes = session.createQuery("FROM Node WHERE id IN (:ids)");
+		queryNodes.setParameter("ids", sources);
+		List<Node> nodes = queryNodes.list();
+		
+		//	Convert both nodes and links to ArrayNodes
+		ObjectMapper mapper = new ObjectMapper();
+		ArrayNode array_node = mapper.valueToTree(nodes);
+		ArrayNode array_link = mapper.valueToTree(links);
+		
+		// 	Add both array_node and array_link to single ObjectNode which is the type of the output of the method
+		ObjectNode base_net = mapper.createObjectNode();
+		base_net.putArray("nodes").addAll(array_node);
+		base_net.putArray("links").addAll(array_link);
+				
+		/*
+		Gson gson = new Gson();
+		JsonObject listToBeSent = new JsonObject();
+		JsonElement jsonNodes = gson.toJsonTree(nodes);
+		JsonElement jsonLinks = gson.toJsonTree(links);
+		listToBeSent.add("nodes", jsonNodes);
+		listToBeSent.add("links", jsonLinks);
+		System.out.println(listToBeSent);	
+		*/	
+		
+		session.getTransaction().commit();
+		session.close();
+		
+		//	Return the output
+		return base_net;
+	}
+}
--- FinNetwork/src/com/finnetwork/views/company_name.java
+++ FinNetwork/src/com/finnetwork/views/company_name.java
@@ -11,8 +11,8 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.finnetwork.controllers.base_network_cntrl;
 
 // Every url with the format of base_url/base_network will be directed to this class
-@Path("/base_network")
-public class base_network {
+@Path("/company_name")
+public class company_name {
 	
 	//Every GET request will be handled by this method
 	@GET
@@ -21,16 +21,16 @@ public class base_network {
 	//	This method producec JSON output
 	@Produces(MediaType.APPLICATION_JSON)
 	//	url parameter year is stored in int variable called year
-	public Response getResponse (@PathParam("param") int year){
+	public Response getResponse (@PathParam("param") int  year){
 		
 		//	Create an instance of base_network_cntrl class
-		base_network_cntrl base_net_cntrl = new base_network_cntrl();
+		base_network_cntrl base_net_cntrl_company = new  base_network_cntrl();
 		
 		//	Call the relevant method with requested year parameter 
-		JsonNode json_baseNet = base_net_cntrl.get_base_network(year);
+		JsonNode json_baseNet1 = base_net_cntrl_company.get_base_network(year);
 		
 		//	Create a Response and return a JSON with status code 200 OK
-		Response response = Response.ok(json_baseNet, MediaType.APPLICATION_JSON).build();		
+		Response response = Response.ok(json_baseNet1, MediaType.APPLICATION_JSON).build();		
 		return response;
 	
 	}
