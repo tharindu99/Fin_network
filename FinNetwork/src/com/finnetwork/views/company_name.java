1e757fbbb69ae7032f948e66b5de95e206209866
kushankavinda <kaavinda92@gmail.lk>
2017-10-09 12:31:50 +0530

    create year wise and compny wise json for given parameters

--- FinNetwork/src/com/finnetwork/views/company_name.java
+++ FinNetwork/src/com/finnetwork/views/company_name.java
@@ -10,28 +10,31 @@ import javax.ws.rs.core.Response;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.finnetwork.controllers.base_network_cntrl;
 
+
 // Every url with the format of base_url/base_network will be directed to this class
 @Path("/company_name")
 public class company_name {
-	
-	//Every GET request will be handled by this method
+
+	// Every GET request will be handled by this method
 	@GET
-	//URLs with base_url/base_network/year format will be handled here
+	// URLs with base_url/base_network/year format will be handled here
 	@Path("/{param}")
-	//	This method producec JSON output
+	// This method producec JSON output
 	@Produces(MediaType.APPLICATION_JSON)
-	//	url parameter year is stored in int variable called year
-	public Response getResponse (@PathParam("param") int  year){
-		
-		//	Create an instance of base_network_cntrl class
-		base_network_cntrl base_net_cntrl_company = new  base_network_cntrl();
-		
-		//	Call the relevant method with requested year parameter 
-		JsonNode json_baseNet1 = base_net_cntrl_company.get_base_network(year);
-		
-		//	Create a Response and return a JSON with status code 200 OK
-		Response response = Response.ok(json_baseNet1, MediaType.APPLICATION_JSON).build();		
+	// url parameter year is stored in int variable called year
+	public Response getResponse(@PathParam("param") String company1) {
+		int year=2014;
+        String company="BANK OF AMERICA CORP";
+		// Create an instance of base_network_cntrl class
+		base_network_cntrl base_net_cntrl = new base_network_cntrl();
+
+		// Call the relevant method with requested year parameter
+
+		JsonNode json_baseNet = base_net_cntrl.get_base_network(year, company);
+
+		// Create a Response and return a JSON with status code 200 OK
+		Response response = Response.ok(json_baseNet,
+				MediaType.APPLICATION_JSON).build();
 		return response;
-	
 	}
 }
