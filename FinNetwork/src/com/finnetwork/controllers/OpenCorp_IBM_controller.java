81398e96f795076976504a3069aa614d880a88b5
tharindu99 <madushanka.best@gmail.com>
2017-12-07 16:58:48 +0530

    node and link maping problem resolved

--- FinNetwork/src/com/finnetwork/controllers/OpenCorp_IBM_controller.java
+++ FinNetwork/src/com/finnetwork/controllers/OpenCorp_IBM_controller.java
@@ -1,5 +1,6 @@
 package com.finnetwork.controllers;
 
+import java.util.Iterator;
 import java.util.List;
 
 import org.hibernate.Session;
@@ -20,21 +21,33 @@ public class OpenCorp_IBM_controller {
 		Session session = hibernate_util.getSessionFactory().openSession();
 		session.beginTransaction();		
 		
-		Query subjectQuery = session.createQuery("SELECT DISTINCT subject_id AS id, subject_entity_name AS equity FROM OpenCorp_IBM");
+		Query subjectQuery = session.createQuery("SELECT  NEW com.finnetwork.models.OpenCorp_IBM_Node(subject_id AS id, subject_entity_name AS equity) FROM OpenCorp_IBM",OpenCorp_IBM_Node.class);
 		List<OpenCorp_IBM_Node> subjectList = subjectQuery.list();
-		System.out.println(subjectList.size());
 		
-		Query objectQuery = session.createQuery("SELECT object_id AS id, object_entity_name AS equity FROM OpenCorp_IBM");
+		
+		Query objectQuery = session.createQuery("SELECT NEW com.finnetwork.models.OpenCorp_IBM_Node(object_id AS id, object_entity_name AS equity) FROM OpenCorp_IBM");
 		List<OpenCorp_IBM_Node> objectList = objectQuery.list();
 		System.out.println(objectList.size());
 		
 		// nodes
 		subjectList.removeAll(objectList);
 		subjectList.addAll(objectList);
+		
+		/*Iterator itr = subjectList.iterator();
+		while(itr.hasNext()){
+		   Object[] obj = (Object[]) itr.next();
+		   //now you have one array of Object for each row
+		   String client = String.valueOf(obj[0]);  
+		   String service = String.valueOf(obj[1]); 
+		   
+		   
+		   System.out.println(client+"  "+service);
+		}
+		*/
 		System.out.println("final " + subjectList.size());
 		
 		// links
-		Query linkQuery = session.createQuery("SELECT subject_id AS source, object_id AS target FROM OpenCorp_IBM");
+		Query linkQuery = session.createQuery("SELECT NEW com.finnetwork.models.OpenCorp_IBM_Link(subject_id AS source, object_id AS target) FROM OpenCorp_IBM");
 		List<OpenCorp_IBM_Link> connectionList = linkQuery.list();
 		System.out.println("links " + connectionList.size());
 		
